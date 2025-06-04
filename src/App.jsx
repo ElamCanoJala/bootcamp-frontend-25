bcf074538f5bda3e6d4c93555b13a6c9317e65ef
Elam Cano <elamcano16@gmail.com>
2025-06-04 17:07:27 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Seat from "./components/Seats";
 
 function App() {
   let seat = 0;
-  let row = A;
   return (
     <div style={{ display: "flex", gap: "10px" }}>
       <Seat color="red" />
