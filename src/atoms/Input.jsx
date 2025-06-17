481fcd5abead2ac9a31458bbb34fd7af14732f3e
Elam Cano <elamcano16@gmail.com>
2025-06-17 17:20:15 -0300

    add buttons when look for a city

--- src/atoms/Input.jsx
+++ src/atoms/Input.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-const Input = ({ reference }) => {
-  return (
-    <input type="text" placeholder="Search for a city..." ref={reference} />
-  );
+const Input = ({ reference, placeholder }) => {
+  return <input type="text" placeholder={placeholder} ref={reference} />;
 };
 
 export default Input;
--- src/molecules/Search.jsx
+++ src/molecules/Search.jsx
@@ -4,58 +4,46 @@ import { mockWeatherData } from "../assets/mockData";
 
 const Search = () => {
   const inputRef = useRef(null);
-  const [weather, setWeather] = useState(null);
-  const [history, setHistory] = useState([]);
+  const [cityWeather, setCityWeather] = useState({
+    cities: {},
+    selectedCity: null,
+  });
 
   const handleSearch = () => {
-    const city = inputRef.current?.value?.trim();
-    if (!city) {
-      console.log("City not provided");
+    const city = inputRef.current?.value;
+
+    if (!city || !mockWeatherData[city]) {
+      alert("City not found");
       return;
     }
 
-    const data = mockWeatherData[city];
-    if (data) {
-      setWeather({ city, ...data });
-
-      setHistory((prev) => [...prev, city]);
-    } else {
-      setWeather(null);
-      alert("Ciudad no encontrada.");
-    }
+    setCityWeather((prev) => ({
+      cities: {
+        ...prev.cities,
+        [city]: mockWeatherData[city],
+      },
+      selectedCity: city,
+    }));
   };
 
-  const handleHistoryClick = (city) => {
-    const data = mockWeatherData[city];
-    if (data) {
-      setWeather({ city, ...data });
-    }
+  const handleSelectCity = (city) => {
+    setCityWeather((prev) => ({
+      ...prev,
+      selectedCity: city,
+    }));
   };
 
   return (
     <div>
-      <Input reference={inputRef} />
+      <Input reference={inputRef} placeholder="Search for a city..." />
       <ButtonSearch handleSearch={handleSearch} />
-      {weather && (
-        <div>
-          <h2>{weather.city}</h2>
-          <p>Temperatura: {weather.temperature}</p>
-          <p>Humidity: {weather.humidity}</p>
-          <p>Wind Speed: {weather.windSpeed}</p>
-        </div>
-      )}
-      {history.length > 0 && (
-        <div>
-          <h3>Historial:</h3>
-          <div>
-            {history.map((city) => (
-              <button key={city} onClick={() => handleHistoryClick(city)}>
-                {city}
-              </button>
-            ))}
-          </div>
-        </div>
-      )}
+      <div>
+        {Object.keys(cityWeather.cities).map((city) => (
+          <button key={city} onClick={() => handleSelectCity(city)}>
+            {city}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
