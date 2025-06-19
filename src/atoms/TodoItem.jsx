6379e2e718e6724ba392ac4844b38ace11d148c7
Elam Cano <elamcano16@gmail.com>
2025-06-19 17:24:34 -0300

    add todoitem

--- src/atoms/TodoItem.jsx
+++ src/atoms/TodoItem.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoItem = () => {
-  return <div></div>;
+const TodoItem = ({ todo, onToggle, onDelete, onEdit }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState(todo.text);
+
+  const handleEdit = () => {
+    if (isEditing) {
+      onEdit(todo.id, editText);
+    }
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <div style={{ display: "flex", gap: "2px", margin: "7px" }}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => onToggle(todo.id)}
+      />
+      {isEditing ? (
+        <input value={editText} onChange={(e) => setEditText(e.target.value)} />
+      ) : (
+        <span
+          style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+        >
+          {todo.text}
+        </span>
+      )}
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </div>
+  );
 };
 
 export default TodoItem;
